0119e8b872ab03e3f8a28ea8ba85d21f38a42856
Alex Dent <adent@patriotsoftware.com>
2021-07-12 13:57:05 -0400

    Initial commit for action

---
+++ action.ps1
@@ -0,0 +1,14 @@
+dotnet tool install -g Jetbrains.Resharper.GlobalTools
+
+#Add check for correct solution name
+jb inspectcode ${INPUTS_SOLUTION_NAME} --exclude=${INPUTS_EXCLUDE_LIST} -s=${INPUTS_SEVERITY_LEVEL} -o=inspections.xml
+
+[xml]$inspections = [xml](Get-Content -Path .\inspections.xml)
+$errorIssueTypes = $inspections.Report.IssueTypes.IssueType | ? { $_.Severity -eq ${INPUTS_SEVERITY_LEVEL} } | % { $_.Id }
+$errors = $inspections.Report.Issues.Project.Issue | ? { $errorIssueTypes -Contains $_.TypeId }
+if ($errors.Count -ne 0 )
+{
+    echo "FAIL - Identified $($errors.Count) errors in solution."
+    $errors | % { echo "ERROR - $($_.ParentNode.Name) - $($_.TypeId) - $($_.File) line $($_.Line) - $($_.Message)" }
+    exit 1
+}
